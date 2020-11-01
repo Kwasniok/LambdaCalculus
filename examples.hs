0184d67dd287ef74f3f351942465a228dc5373b9
Kwasniok <Kwasniok@users.noreply.github.com>
2020-11-01 20:50:12 +0100

    simplify examples

--- examples.hs
+++ examples.hs
@@ -10,15 +10,13 @@ neval e n = iterate eval e !! n
 nevals :: String -> Int -> Expression
 nevals s n = neval (fromJust (parseWord s)) n
 
-mkD :: String -> Designator
-mkD s = Designator s
+mkD :: String -> Expression
+mkD s = ExpressionTerm (TermDesignator (Designator s))
 
-dToE :: Designator -> Expression
-dToE d = ExpressionTerm (TermDesignator d)
-
-lambda :: Designator -> [Expression] -> Expression
-lambda d [e] = ExpressionTerm (TermFunction d e)
-lambda d es = ExpressionTerm (TermFunction d (ExpressionList es))
+-- note: first argument must be a designator
+lambda :: Expression -> [Expression] -> Expression
+lambda (ExpressionTerm (TermDesignator d)) [e] = ExpressionTerm (TermFunction d e)
+lambda (ExpressionTerm (TermDesignator d)) es = ExpressionTerm (TermFunction d (ExpressionList es))
 
 a = mkD "a"
 b = mkD "b"
@@ -28,25 +26,25 @@ x = mkD "x"
 y = mkD "y"
 z = mkD "z"
 
-lid = lambda x [dToE x]
+lid = lambda x [x]
 
 -- boolean algebra
-lTrue = lambda x [lambda y [dToE x, dToE y]]
-lFalse = lambda x [lambda y [dToE y, dToE x]]
+lTrue = lambda x [lambda y [x, y]]
+lFalse = lambda x [lambda y [y, x]]
 lNot = lFalse
-lIf = lambda x [lambda y [lambda z [dToE x, dToE y, dToE z]]]
-lAnd = lambda x [lambda y [lambda z [lambda a [dToE x, dToE y, dToE x, dToE z, dToE a]]]]
-lNand = lambda x [lambda y [lambda z [lambda a [dToE x, dToE y, dToE x, dToE a, dToE z]]]]
+lIf = lambda x [lambda y [lambda z [x, y, z]]]
+lAnd = lambda x [lambda y [lambda z [lambda a [x, y, x, z, a]]]]
+lNand = lambda x [lambda y [lambda z [lambda a [x, y, x, a, z]]]]
 
 -- schoenfield and more
-lI = lambda x [dToE x]
-lK = lambda x [lambda y [dToE x]]
-lZ = lambda x [lambda y [lambda z [dToE x, ExpressionList[dToE y, dToE z]]]]
-lS = lambda x [lambda y [lambda z [ExpressionList[dToE x, dToE z], ExpressionList [dToE y, dToE z]]]]
-lT = lambda x [lambda y [lambda z [dToE x, dToE z, dToE y]]]
-lY = lambda y [lambda x [dToE y, dToE x, dToE x], lambda x [dToE y, dToE x, dToE x]]
+lI = lambda x [x]
+lK = lambda x [lambda y [x]]
+lZ = lambda x [lambda y [lambda z [x, ExpressionList[y, z]]]]
+lS = lambda x [lambda y [lambda z [ExpressionList[x, z], ExpressionList [y, z]]]]
+lT = lambda x [lambda y [lambda z [x, z, y]]]
+lY = lambda y [lambda x [y, x, x], lambda x [y, x, x]]
 
-lInfexpand = lambda y [lambda x [dToE y, dToE x, dToE x, dToE x], lambda x [dToE y, dToE x, dToE x, dToE x]]
+lInfexpand = lambda y [lambda x [y, x, x, x], lambda x [y, x, x, x]]
 
 lIfTrue = ExpressionList [lIf, lTrue]
 lIfFalse = ExpressionList [lIf, lFalse]
